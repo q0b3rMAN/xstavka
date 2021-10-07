55002fb336214311f437c32594f467607c8c1039
skanfork <92070126+skanfork@users.noreply.github.com>
2021-10-07 08:29:40 +0300

    1.1

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
+    $start = microtime(true);
 
     // Модуль получения данных с xstavka
+    // https://scanfork.org
 
     require __DIR__ . '/lib/RollingCurl.php';
     require __DIR__ . '/lib/Request.php';
@@ -19,7 +21,7 @@
  
     curl_close($ch);
     fclose($fp);
- 
+    
     // Получаем данные по каждому матчу
     $json = file_get_contents('data/xstavka_list.json');
     $ids = json_decode($json, true);
@@ -31,17 +33,94 @@
         $rc->get('https://1xstavka.ru/LiveFeed/GetGameZip?id='.$value['I']); // Формируем очередь запросов  
     }
     
-    $result = []; //забудьте про array(), это очень старая фигня
-    $rc->setCallback(function(\RollingCurl\Request $request, \RollingCurl\RollingCurl $rollingCurl) { 
+    $result = [];
+    $rc->setCallback(function(\RollingCurl\Request $request, \RollingCurl\RollingCurl $rollingCurl) use (&$result) { 
     $t = json_decode($request->getResponseText());
-        $new = []; //не забудьте объявить $new
-        foreach($t as $key=>$value) {
-           $new[$key]['ID'] = $value->I;  
-        }
-        $result[] = $new; //в массив $result мы добавляем массив $new
         //print_r($t);
-       return $result;
+        $new = [];
+
+        // Формируем свой массив
+        foreach($t as $key=>$value) {
+
+            $new['ID'] = $value->I; // ID матча
+            $new['Sport'] = $value->SN; // Вид спорта
+            $new['League'] = $value->L; // Лига
+            $new['Home'] = $value->O1; // Хозяева
+            $new['Away'] = $value->O2; // Гости       
+
+            // Создаем ссылку на матч
+            $SE = str_replace('.', '', $value->SE);
+            $SE = str_replace(array('(',')','+','/'), array('','','',''), $SE);          
+            $SE = preg_replace('![\s]+!', "-" , $SE);
+
+            $LE = str_replace('.', '', $value->LE);
+            $LE = str_replace(array('(',')','+','/'), array('','','',''), $LE);                       
+            $LE = preg_replace('![\s]+!', "-" , $LE);
+
+            $HOME = str_replace('.', '', $value->O1E);
+            $HOME = str_replace(array('(',')','+','/'), array('','','',''), $HOME); 
+            $HOME = preg_replace('![\s]+!', "-" , $HOME);
+
+            $AWAY = str_replace('.', '', $value->O2E);
+            $AWAY = str_replace(array('(',')','+','/'), array('','','',''), $AWAY);           
+            $AWAY = preg_replace('![\s]+!', "-" , $AWAY); 
+            
+            $new['Href'] = 'https://1xstavka.ru/live/'.$SE.'/'.$value->LI.'-'.$LE.'/'.$value->I.'-'.$HOME.'-'.$AWAY.'/';
+
+            // Если есть коээфиценты
+            if (isset($value->E)) {
+                foreach ($value->E as $key_c => $value_c) {
+
+                    // У меня свое обозначение маркетов, так удобнее
+                    if ($value_c->T == 1) {
+                        $new['Odds'][1]['MarketType'] = $value_c->T; // Тип
+                        $new['Odds'][1]['MarketName'] = 'П1'; // Название
+                        $new['Odds'][1]['Coefficient'] = $value_c->C; // КФ
+                    }
+
+                    if ($value_c->T == 2) {
+                        $new['Odds'][2]['MarketType'] = $value_c->T;
+                        $new['Odds'][2]['MarketName'] = 'Ничья';
+                        $new['Odds'][2]['Coefficient'] = $value_c->C;
+                    } 
+                    
+                    if ($value_c->T == 3) {
+                        $new['Odds'][3]['MarketType'] = $value_c->T;
+                        $new['Odds'][3]['MarketName'] = 'П2';
+                        $new['Odds'][3]['Coefficient'] = $value_c->C;
+                    }
+                    
+                    if ($value_c->T == 4) {
+                        $new['Odds'][4]['MarketType'] = $value_c->T;
+                        $new['Odds'][4]['MarketName'] = '1X';
+                        $new['Odds'][4]['Coefficient'] = $value_c->C;
+                    }   
+                    
+                    if ($value_c->T == 5) {
+                        $new['Odds'][5]['MarketType'] = $value_c->T;
+                        $new['Odds'][5]['MarketName'] = '12';
+                        $new['Odds'][5]['Coefficient'] = $value_c->C;
+                    }  
+                    
+                    if ($value_c->T == 6) {
+                        $new['Odds'][6]['MarketType'] = $value_c->T;
+                        $new['Odds'][6]['MarketName'] = '2X';
+                        $new['Odds'][6]['Coefficient'] = $value_c->C;
+                    }   
+                }               
+            }
+        }
+
+        $result[] = $new;
+        return $result;
     });
-    $rs->execute();
+    
+    $rc->execute(); // Запускаем
+    
+    // echo '<pre>';
     //print_r($result);
 
+    // Запишем результаты в файл
+    if(file_put_contents('data/xstavka_result.json', json_encode($result))) {
+        echo 'Status OK! Results processed in '; echo $time = microtime(true) - $start;
+    }
